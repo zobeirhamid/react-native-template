6eade3f8652b1a2cf756b0e95bda18bf1b9e369e
Zobeir Hamid <zobeirhamid@berkeley.edu>
2021-08-29 13:33:58 -0700

    Adds react-native-web

---
+++ template/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: path.resolve(__dirname, './public/index.html'),
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: ['babel-polyfill', path.join(__dirname, 'index.web.js')],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js|mjs)$/,
+        exclude: /node_modules\/(?!()\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-flow',
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+              {
+                plugins: [
+                  '@babel/plugin-proposal-class-properties',
+                  'babel-plugin-react-native-web',
+                ],
+              },
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|mp4)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.web.tsx', '.tsx', '.web.ts', '.ts', '.web.js', '.js'],
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+  },
+
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.DefinePlugin({
+      process: {env: {}},
+      __DEV__: process.env.NODE_ENV === 'development',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true,
+  },
+};
