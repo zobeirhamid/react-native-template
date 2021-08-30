7c3f0c70bf89237dd02be9e7d462e11118e1036f
Zobeir Hamid <zobeirhamid@berkeley.edu>
2021-08-29 23:19:20 -0700

    Fixes Image issues

--- template/src/library/components/Image/Image.tsx
+++ template/src/library/components/Image/Image.tsx
@@ -1,15 +1,3 @@
-import React from 'react';
 import FastImage, {FastImageProps} from 'react-native-fast-image';
-import withFadeInProp, {
-  FadeInProp,
-} from 'library/higherOrderComponents/withFadeInProp';
 
-export type ImageProps = FastImageProps & FadeInProp;
-
-const Image: React.FC<ImageProps> = props => {
-  return <FastImage {...props} />;
-};
-
-Image.displayName = 'Image';
-
-export default Image;
+export default FastImage;
--- template/src/library/components/Image/Image.web.tsx
+++ template/src/library/components/Image/Image.web.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import {Image as RNImage, ImageProperties} from 'react-native';
-import withFadeInProp, {
-  FadeInProp,
-} from 'library/higherOrderComponents/withFadeInProp';
-
-export type ImageProps = ImageProperties & FadeInProp;
-
-const Image: React.FC<ImageProps> = props => {
-  return <RNImage {...props} />;
-};
-
-Image.displayName = 'Image';
-
+import {Image} from 'react-native';
 export default Image;
